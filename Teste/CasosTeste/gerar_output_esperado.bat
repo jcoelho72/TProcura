54b850eda45de2fd1860222b2c8b8a88e3f1e183
jcoelho72 <jcoelho@uab.pt>
2025-05-15 13:04:30 +0100

    Geração do ficheiro output_esperado.txt em UTF8 no windows.

--- Teste/CasosTeste/gerar_output_esperado.bat
+++ Teste/CasosTeste/gerar_output_esperado.bat
@@ -1,3 +1,3 @@
-chcp 65001
-cmd /c "..\..\x64\Debug\TProcuraConstrutiva.exe < input.txt > output_esperado.txt"
-pause
+cmd /c "..\..\x64\Debug\TProcuraConstrutiva.exe < input.txt > output.txt"
+powershell -Command "Get-Content output.txt | Set-Content -Encoding utf8 output_esperado.txt"
+del output.txt
