6f39d70c4dea3062e7aee3ae56da80eba6a4ef26
jcoelho72 <jcoelho@uab.pt>
2025-07-06 08:31:13 +0000

    Deploying to gh-pages from @ jcoelho72/TProcura@ea51e82e0e1daffd03b3002dedd7f93c8773a4c3 🚀

--- group__RedefinicaoOpcional.js
+++ group__RedefinicaoOpcional.js
@@ -3,6 +3,6 @@ var group__RedefinicaoOpcional =
     [ "TProcuraConstrutiva::Acao", "group__RedefinicaoOpcional.html#gacccf4fbb52126ba19bfc456c0c6d5214", null ],
     [ "TProcuraConstrutiva::Distinto", "group__RedefinicaoOpcional.html#ga1cf2a30e496ea869e85a9d60b4d469e9", null ],
     [ "TProcuraConstrutiva::ExecutaAlgoritmo", "group__RedefinicaoOpcional.html#gae52871eb18c8ae656a2174acf43b3bb3", null ],
-    [ "TProcuraConstrutiva::Indicador", "group__RedefinicaoOpcional.html#ga8828e1506e7b7713eb226681f117be10", null ],
+    [ "TProcuraConstrutiva::Indicador", "group__RedefinicaoOpcional.html#gad8bb028ef31b04b7932be22ca0afbd3c", null ],
     [ "TProcuraConstrutiva::MostrarSolucao", "group__RedefinicaoOpcional.html#ga1fdecf86b41c94a1656c17495f9b5b93", null ]
 ];
