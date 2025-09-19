580e066db7aed9df274789539a5d1d92a9e97b90
jcoelho72 <jcoelho@uab.pt>
2025-09-19 19:23:59 +0100

    Operadores das classes codificadas, não tinham dependência respetiva para EM e AG/AE.

--- Melhorativa/TCodificacaoPermutacao.cpp
+++ Melhorativa/TCodificacaoPermutacao.cpp
@@ -39,13 +39,13 @@ void TCodificacaoPermutacao::ResetParametros() {
 
 	// parametros da codificação inteira
 	parametro += {
-		{ "TIPO_CRUZAR", 3, 1, 4, "TIPO_CRUZAR: 1 - PMX, 2 - Edge, 3 - Order; 4 - Cycle", nomesCruzamento },
-		{ "TIPO_MUTAR", 0,0,0, "TIPO_MUTAR: 0 - aplica um vizinho aleatório (seja 1 só elemento ou segmento)", NULL },
-		{ "TIPO_VIZINHO", 1,1,3, "TIPO_VIZINHO: vários métodso para vizinhanças de inteiros", nomesVizinhanca },
+		{ "TIPO_CRUZAR", 3, 1, 4, "TIPO_CRUZAR: 1 - PMX, 2 - Edge, 3 - Order; 4 - Cycle", nomesCruzamento, _TV("0,2,3") },
+		{ "TIPO_MUTAR", 0,0,0, "TIPO_MUTAR: 0 - aplica um vizinho aleatório (seja 1 só elemento ou segmento)", NULL, _TV("0,2,3") },
+		{ "TIPO_VIZINHO", 1,1,3, "TIPO_VIZINHO: vários métodso para vizinhanças de inteiros", nomesVizinhanca, _TV("0,1") },
 		{ "LIMITE_VIZINHOS", 0,0,1000,
 "LIMITE_VIZINHOS, conforme a vizinhança, se 0 não há limite\n\
 - inserir + trocaPar + inverterSegmento - limita a distância entre pares", NULL },
-		{ "TIPO_DISTANCIA", 1,1,3, "Distância: vários métodso para distâncias de permutações", nomesDistancias }
+		{ "TIPO_DISTANCIA", 1,1,3, "Distância: vários métodso para distâncias de permutações", nomesDistancias, _TV("0,2,3") }
 	};
 }
 
