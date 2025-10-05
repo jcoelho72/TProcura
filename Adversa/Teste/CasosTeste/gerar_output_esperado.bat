d95e9d0ea1fb9b5cf7d55f82ec1ee70a76376d86
jcoelho72 <jcoelho@uab.pt>
2025-10-05 19:00:11 +0100

    casos de teste atualizados

--- Adversa/Teste/CasosTeste/gerar_output_esperado.bat
+++ Adversa/Teste/CasosTeste/gerar_output_esperado.bat
@@ -1,3 +1,3 @@
-cmd /c "..\..\x64\Debug\TProcuraAdversa.exe < input.txt > output1.txt"
+cmd /c "..\x64\Debug\TProcuraAdversa.exe < input.txt > output1.txt"
 powershell -Command "Get-Content output1.txt | Set-Content -Encoding utf8 output.txt"
 del output1.txt
--- Construtiva/Teste/CasosTeste/gerar_output_esperado.bat
+++ Construtiva/Teste/CasosTeste/gerar_output_esperado.bat
@@ -1,3 +1,3 @@
-cmd /c "..\..\..\x64\Debug\TProcuraConstrutiva.exe < inputAspirador.txt > output.txt"
+cmd /c "..\x64\Debug\TProcuraConstrutiva.exe < inputAspirador.txt > output.txt"
 powershell -Command "Get-Content output.txt | Set-Content -Encoding utf8 outputAspirador.txt"
 del output.txt
