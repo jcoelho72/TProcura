6eae5c8189ede1978431043e38dd07db8ceb6e98
jcoelho72 <jcoelho@uab.pt>
2025-10-21 13:23:06 +0000

    Deploying to gh-pages from @ jcoelho72/TProcura@1afef140008cf6e5ba5c6b5882f422f069f790f7 🚀

--- search/all_15.js
+++ search/all_15.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['largura_0',['largura',['../teste_puzzle8.html#autotoc_md78',1,'Ação 3 - Procura em Largura'],['../teste_8damas.html#autotoc_md23',1,'Ação 3 - Procura em Largura'],['../teste_aspirador1.html#autotoc_md44',1,'Ação 5 - Efetuer uma procura em Largura'],['../teste_aspirador1.html#autotoc_md48',1,'Ação 9 - Desafio Procura em Largura']]],
+  ['largura_0',['largura',['../teste_puzzle8.html#autotoc_md81',1,'Ação 3 - Procura em Largura'],['../teste_8damas.html#autotoc_md23',1,'Ação 3 - Procura em Largura'],['../teste_aspirador1.html#autotoc_md44',1,'Ação 5 - Efetuer uma procura em Largura'],['../teste_aspirador1.html#autotoc_md48',1,'Ação 9 - Desafio Procura em Largura']]],
   ['largura_5fprimeiro_1',['LARGURA_PRIMEIRO',['../TProcuraConstrutiva_8h.html#a80645d45a22bcc4a9a5a384051349d6ca4bdb9cfb386855ab9d6bc3e2a369f6ed',1,'TProcuraConstrutiva.h']]],
   ['larguraprimeiro_2',['LarguraPrimeiro',['../group__ProcurasCegas.html#gafe656922894d8a27b4cfb70128793af0',1,'TProcuraConstrutiva']]],
   ['last_3',['Last',['../classTVector.html#aa304d639c79ef2842f9951846c6292e3',1,'TVector']]],
   ['ler_4',['LER',['../TProcura_8h.html#a10f5ddbb9232fda16b29e31829ac51d3ae6fe9228f4939ae8277bb49921020a46',1,'TProcura.h']]],
   ['libertarvector_5',['libertarvector',['../classTProcuraConstrutiva.html#a976f17b2b53ce9ffc7d1795d291262df',1,'TProcuraConstrutiva::LibertarVector()'],['../classTProcuraMelhorativa.html#a28993c2953d882b21f3c456e5d1b7e47',1,'TProcuraMelhorativa::LibertarVector()']]],
-  ['licença_6',['Licença',['../index.html#autotoc_md130',1,'']]],
+  ['licença_6',['Licença',['../index.html#autotoc_md133',1,'']]],
   ['limite_7',['LIMITE',['../TProcuraConstrutiva_8h.html#a3c6c902bf1e6a69c702caca172718058a32d02498ce9b0c0377275e54839bfad2',1,'TProcuraConstrutiva.h']]],
   ['limite_5fiteracoes_8',['LIMITE_ITERACOES',['../TProcura_8h.html#ab9850fa0e8c4a6f8112bf071778c1dc6a59eb50b8b96453c7ff04c538a27b52bd',1,'TProcura.h']]],
   ['limite_5ftempo_9',['LIMITE_TEMPO',['../TProcura_8h.html#ab9850fa0e8c4a6f8112bf071778c1dc6ad4d1ad2d967b020f5527e13a8db8a1bc',1,'TProcura.h']]],
@@ -15,7 +15,7 @@ var searchData=
   ['limparestatisticas_12',['limparestatisticas',['../classTProcura.html#ae82e7be02923531e7ead26b1b25cec0f',1,'TProcura::LimparEstatisticas()'],['../classTProcuraMelhorativa.html#a6c7163b20ace9bebdf3c073cd475dec9',1,'TProcuraMelhorativa::LimparEstatisticas()'],['../classTProcuraConstrutiva.html#adaefadabea2eb225ed28c3927feb6828',1,'TProcuraConstrutiva::LimparEstatisticas() override']]],
   ['limparht_13',['LimparHT',['../classTProcuraConstrutiva.html#a8b500987c88352d27a68200454cc3c8c',1,'TProcuraConstrutiva']]],
   ['linha_14',['Teste Jogo Em Linha',['../teste_jogo_em_linha.html',1,'']]],
-  ['linha_20de_20comando_15',['Ação 7: Linha de comando',['../teste_tvector.html#autotoc_md101',1,'']]],
+  ['linha_20de_20comando_15',['Ação 7: Linha de comando',['../teste_tvector.html#autotoc_md104',1,'']]],
   ['lower_5fbound_16',['LOWER_BOUND',['../TProcuraAdversa_8h.html#af2d8f3ce278d837a36ee4d610662b204a7b49753da05e5513632fc19383072793',1,'TProcuraAdversa.h']]],
   ['lowerbound_17',['lowerbound',['../classTProcuraConstrutiva.html#a5f021041a7dfe1b68890d9e5f5f0e6f4',1,'TProcuraConstrutiva::LowerBound()'],['../classTProcuraMelhorativa.html#ab1191e46286698e517e2d5bf3cd18ff0',1,'TProcuraMelhorativa::lowerBound'],['../group__VariaveisGlobais.html#ga1af840b6ec9c04ce7d4c8849346441bd',1,'TProcuraConstrutiva::lowerBound']]]
 ];
--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['9_20branch_20and_20bound_0',['Ação 9 - Branch-and-Bound',['../teste_puzzle8.html#autotoc_md84',1,'']]],
+  ['9_20branch_20and_20bound_0',['Ação 9 - Branch-and-Bound',['../teste_puzzle8.html#autotoc_md87',1,'']]],
   ['9_20desafio_20procura_20em_20largura_1',['Ação 9 - Desafio Procura em Largura',['../teste_aspirador1.html#autotoc_md48',1,'']]]
 ];
