6afd204d634cf3cff1be4cecc75545e946a38232
jcoelho72 <jcoelho@uab.pt>
2025-10-26 18:46:32 +0000

    Deploying to gh-pages from @ jcoelho72/TProcura@b106c497950e47e13fc1adc99b96d0144e832d7b 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['2_0',['Teste Aspirador - Parte 1/2',['../teste_aspirador1.html',1,'']]],
   ['2_202_1',['Teste Aspirador - Parte 2/2',['../teste_aspirador2.html',1,'']]],
-  ['2_20download_20manual_2',['Opção 2 - Download Manual',['../index.html#autotoc_md121',1,'']]],
-  ['2_20explorar_20os_20sucessores_3',['Ação 2 - Explorar os sucessores',['../teste_aspirador1.html#autotoc_md38',1,'']]],
-  ['2_20explorar_20sucessores_4',['Ação 2 - Explorar sucessores',['../teste_jogo_do_galo.html#autotoc_md55',1,'']]],
-  ['2_20heurística_5',['Ação 2 - Heurística',['../teste_jogo_em_linha.html#autotoc_md62',1,'']]],
-  ['2_20resolver_20manualmente_6',['2 resolver manualmente',['../teste_8damas.html#autotoc_md22',1,'Ação 2 - Resolver manualmente'],['../teste_8damas_ci.html#autotoc_md32',1,'Ação 2 - Resolver manualmente'],['../teste_particao.html#autotoc_md69',1,'Ação 2 - Resolver manualmente'],['../teste_puzzle8.html#autotoc_md77',1,'Ação 2 - Resolver manualmente']]],
-  ['2_3a_20menu_203_20parâmetros_7',['Ação 2: Menu 3 - Parâmetros',['../teste_tvector.html#autotoc_md91',1,'']]]
+  ['2_20download_20manual_2',['Opção 2 - Download Manual',['../index.html#autotoc_md124',1,'']]],
+  ['2_20explorar_20os_20sucessores_3',['Ação 2 - Explorar os sucessores',['../teste_aspirador1.html#autotoc_md41',1,'']]],
+  ['2_20explorar_20sucessores_4',['Ação 2 - Explorar sucessores',['../teste_jogo_do_galo.html#autotoc_md58',1,'']]],
+  ['2_20heurística_5',['Ação 2 - Heurística',['../teste_jogo_em_linha.html#autotoc_md65',1,'']]],
+  ['2_20resolver_20manualmente_6',['2 resolver manualmente',['../teste_8damas.html#autotoc_md22',1,'Ação 2 - Resolver manualmente'],['../teste_8damas_ci.html#autotoc_md32',1,'Ação 2 - Resolver manualmente'],['../teste_particao.html#autotoc_md72',1,'Ação 2 - Resolver manualmente'],['../teste_puzzle8.html#autotoc_md80',1,'Ação 2 - Resolver manualmente']]],
+  ['2_3a_20estratégia_20de_20funcionamento_7',['Ação 2: Estratégia de funcionamento',['../deucalion.html#autotoc_md4',1,'']]],
+  ['2_3a_20menu_203_20parâmetros_8',['Ação 2: Menu 3 - Parâmetros',['../teste_tvector.html#autotoc_md94',1,'']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['6_20editar_20opções_20a_0',['Ação 6 - Editar opções A',['../teste_aspirador1.html#autotoc_md42',1,'']]],
-  ['6_20executar_1',['Ação 3: Menu 6 - Executar',['../teste_tvector.html#autotoc_md93',1,'']]],
-  ['6_20melhor_20primeiro_2',['Ação 6 - Melhor Primeiro',['../teste_puzzle8.html#autotoc_md81',1,'']]],
-  ['6_20testes_20empíricos_3',['6 testes empíricos',['../teste_8damas.html#autotoc_md26',1,'Ação 6 - Testes Empíricos'],['../teste_jogo_do_galo.html#autotoc_md59',1,'Ação 6 - Testes Empíricos']]],
-  ['6_20torneio_20poda_4',['Ação 6 - Torneio Poda',['../teste_jogo_em_linha.html#autotoc_md66',1,'']]],
-  ['6_3a_20menu_208_20teste_5',['Ação 6: Menu 8 - Teste',['../teste_tvector.html#autotoc_md99',1,'']]]
+  ['6_20editar_20opções_20a_0',['Ação 6 - Editar opções A',['../teste_aspirador1.html#autotoc_md45',1,'']]],
+  ['6_20executar_1',['Ação 3: Menu 6 - Executar',['../teste_tvector.html#autotoc_md96',1,'']]],
+  ['6_20melhor_20primeiro_2',['Ação 6 - Melhor Primeiro',['../teste_puzzle8.html#autotoc_md84',1,'']]],
+  ['6_20testes_20empíricos_3',['6 testes empíricos',['../teste_8damas.html#autotoc_md26',1,'Ação 6 - Testes Empíricos'],['../teste_jogo_do_galo.html#autotoc_md62',1,'Ação 6 - Testes Empíricos']]],
+  ['6_20torneio_20poda_4',['Ação 6 - Torneio Poda',['../teste_jogo_em_linha.html#autotoc_md69',1,'']]],
+  ['6_3a_20menu_208_20teste_5',['Ação 6: Menu 8 - Teste',['../teste_tvector.html#autotoc_md102',1,'']]]
 ];
