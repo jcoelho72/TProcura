f0f0b494b37c4714dbb980964e777f8f3515494a
jcoelho72 <jcoelho@uab.pt>
2025-10-28 14:10:12 +0000

    MPI

--- Teste/tvetor.sh
+++ Teste/tvetor.sh
@@ -17,25 +17,25 @@ make mpi || { echo "Compilação falhou"; exit 1; }
 # Teste 1
 
 # esforço A
-srun ./bin/mpi/TProcura "1:10" -R Resultados/tvetor_1m0 -M 0 -P P1=2 P2=3 P3=1
-srun ./bin/mpi/TProcura "1:10" -R Resultados/tvetor_1 -M 1 -P P1=2 P2=3 P3=1
+srun bin/MPI/TProcura "1:10" -R Resultados/tvetor_1m0 -M 0 -P P1=2 P2=3 P3=1
+srun bin/MPI/TProcura "1:10" -R Resultados/tvetor_1 -M 1 -P P1=2 P2=3 P3=1
 
 # esforço B
-srun ./bin/mpi/TProcura "1:10" -R Resultados/tvetor_1Bmo -M 0 -P P1=2 P2=3 "P3=1:10"
-srun ./bin/mpi/TProcura "1:10" -R Resultados/tvetor_1B -M 1 -P P1=2 P2=3 "P3=1:10"
+srun bin/MPI/TProcura "1:10" -R Resultados/tvetor_1Bmo -M 0 -P P1=2 P2=3 "P3=1:10"
+srun bin/MPI/TProcura "1:10" -R Resultados/tvetor_1B -M 1 -P P1=2 P2=3 "P3=1:10"
 
 # esforço C
-srun ./bin/mpi/TProcura "1:10" -R Resultados/tvetor_1C -M 1 -P P1=2 P2=2 "P3=1:100"
+srun bin/MPI/TProcura "1:10" -R Resultados/tvetor_1C -M 1 -P P1=2 P2=2 "P3=1:100"
 
 # Teste 2
 
 # esforço A
-srun ./bin/mpi/TProcura 1 -R Resultados/tvetor_2mo -M 0 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
-srun ./bin/mpi/TProcura 1 -R Resultados/tvetor_2 -M 1 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
+srun bin/MPI/TProcura 1 -R Resultados/tvetor_2mo -M 0 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
+srun bin/MPI/TProcura 1 -R Resultados/tvetor_2 -M 1 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
 
 # esforço B
-srun ./bin/mpi/TProcura "1:4" -R Resultados/tvetor_2Bmo -M 0 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
-srun ./bin/mpi/TProcura "1:4" -R Resultados/tvetor_2B -M 1 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
+srun bin/MPI/TProcura "1:4" -R Resultados/tvetor_2Bmo -M 0 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
+srun bin/MPI/TProcura "1:4" -R Resultados/tvetor_2B -M 1 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
 
 # esforço C
-srun ./bin/mpi/TProcura "1:10" -R Resultados/tvetor_2C -M 1 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
+srun bin/MPI/TProcura "1:10" -R Resultados/tvetor_2C -M 1 -P P2=3 "P1=1:12" x "P6=1:3" x "P3=1:10"
