3eb034035273e0ec786fd9d2517f64e0bc2a2aad
jcoelho72 <jcoelho@uab.pt>
2025-11-13 16:03:28 +0000

    Deploying to gh-pages from @ jcoelho72/TProcura@d31102deb59ea116c3993b4d50faea3fa667a8d8 🚀

--- search/all_3.js
+++ search/all_3.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['4_20minimax_20com_20cortes_20alfa_20beta_0',['Ação 4 - MiniMax com cortes Alfa/Beta',['../teste_jogo_do_galo.html#autotoc_md67',1,'']]],
-  ['4_20procura_20em_20profundidade_1',['4 procura em profundidade',['../teste_8damas.html#autotoc_md24',1,'Ação 4 - Procura em Profundidade'],['../teste_puzzle8.html#autotoc_md95',1,'Ação 4 - Procura em Profundidade']]],
-  ['4_20testes_20empíricos_2',['4 testes empíricos',['../teste_8damas_ci.html#autotoc_md34',1,'Ação 4 - Testes Empíricos'],['../teste_8damas_cp.html#autotoc_md42',1,'Ação 4 - Testes Empíricos']]],
+  ['4_20procura_20em_20profundidade_1',['4 procura em profundidade',['../teste_8damas.html#autotoc_md24',1,'Ação 4 - Procura em Profundidade'],['../teste_puzzle8.html#autotoc_md96',1,'Ação 4 - Procura em Profundidade']]],
+  ['4_20testes_20empíricos_2',['4 testes empíricos',['../teste_8damas_ci.html#autotoc_md34',1,'Ação 4 - Testes Empíricos'],['../teste_8damas_cp.html#autotoc_md41',1,'Ação 4 - Testes Empíricos']]],
   ['4_20torneio_20iterativo_3',['Ação 4 - Torneio Iterativo',['../teste_jogo_em_linha.html#autotoc_md74',1,'']]],
   ['4_20ver_20a_20alterar_20um_20parâmetro_4',['Ação 4 - Ver a alterar um parâmetro',['../teste_aspirador1.html#autotoc_md50',1,'']]],
-  ['4_3a_20menu_205_20indicadores_5',['Ação 4: Menu 5 - Indicadores',['../teste_tvector.html#autotoc_md111',1,'']]]
+  ['4_3a_20menu_205_20indicadores_5',['Ação 4: Menu 5 - Indicadores',['../teste_tvector.html#autotoc_md112',1,'']]]
 ];
