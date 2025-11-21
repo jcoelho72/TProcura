40729fafd636ac1f17af393e5e233e918551e8e7
jcoelho72 <jcoelho@uab.pt>
2025-11-21 16:23:23 +0000

    Deploying to gh-pages from @ jcoelho72/TProcura@db9e784e87aa8d7322bd01ed279a12dbb444e304 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,11 +1,11 @@
 var searchData=
 [
   ['3_0',['Teste 3',['../melhorativa__deucalion.html#autotoc_md28',1,'']]],
-  ['3_20algoritmos_20evolutivos_1',['3 algoritmos evolutivos',['../teste_8damas_ci.html#autotoc_md78',1,'Ação 3 - Algoritmos Evolutivos'],['../teste_8damas_cp.html#autotoc_md83',1,'Ação 3 - Algoritmos Evolutivos'],['../teste_particao_cb.html#autotoc_md126',1,'Ação 3 - Algoritmos Evolutivos']]],
+  ['3_20algoritmos_20evolutivos_1',['3 algoritmos evolutivos',['../teste_8damas_ci.html#autotoc_md78',1,'Ação 3 - Algoritmos evolutivos'],['../teste_8damas_cp.html#autotoc_md83',1,'Ação 3 - Algoritmos evolutivos'],['../teste_particao_cb.html#autotoc_md126',1,'Ação 3 - Algoritmos evolutivos']]],
   ['3_20minimax_2',['Ação 3 - MiniMax',['../teste_jogo_do_galo.html#autotoc_md105',1,'']]],
   ['3_20parâmetros_3',['Ação 2: Menu 3 - Parâmetros',['../teste_tvector.html#autotoc_md144',1,'']]],
   ['3_20procura_20em_20largura_4',['3 procura em largura',['../teste_8damas.html#autotoc_md68',1,'Ação 3 - Procura em Largura'],['../teste_puzzle8.html#autotoc_md131',1,'Ação 3 - Procura em Largura']]],
-  ['3_20testes_20empíricos_5',['3 testes empíricos',['../teste_particao.html#autotoc_md119',1,'Ação 3 - Testes Empíricos'],['../teste_particao_cb.html#autotoc_md127',1,'Ação 3 - Testes Empíricos']]],
+  ['3_20testes_20empíricos_5',['3 testes empíricos',['../teste_particao.html#autotoc_md119',1,'Ação 3 - Testes Empíricos'],['../teste_particao_cb.html#autotoc_md127',1,'Ação 3 - Testes empíricos']]],
   ['3_20torneio_20profundidade_6',['Ação 3 - Torneio Profundidade',['../teste_jogo_em_linha.html#autotoc_md112',1,'']]],
   ['3_20utilizar_20como_20submódulo_7',['Opção 3 - Utilizar como Submódulo',['../index.html#autotoc_md177',1,'']]],
   ['3_20ver_20a_20solução_20realizada_20manualmente_8',['Ação 3 - Ver a solução realizada manualmente',['../teste_aspirador1.html#autotoc_md88',1,'']]],
