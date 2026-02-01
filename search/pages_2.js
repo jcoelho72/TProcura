d783dee460f9d09f4736672f210b8dc3ac20119d
jcoelho72 <jcoelho@uab.pt>
2026-02-01 15:51:08 +0000

    Deploying to gh-pages from @ jcoelho72/TProcura@7462b7086085159bd5307842e60504d4a06bf5f1 🚀

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['3_0',['Puzzle8, procura em profundidade nível 3',['../md_docs_2tutorial__puzzle8prof.html',1,'']]]
+  ['3_0',['Puzzle8, procura em profundidade nível 3',['../tutorial_puzzle8prof.html',1,'']]]
 ];
